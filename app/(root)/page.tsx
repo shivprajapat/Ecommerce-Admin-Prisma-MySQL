01c89bebf94001c6db28833cce58b6ef4c6b7239
shivprajapat <shivkumarprajapat232@gmail.com>
2023-07-28 10:01:18 +0530

    feat: 🎸 Clerk Authentication (Admin)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="text-center">
       <h1 className="text-lg text-rose-700">This is a Protected Route!</h1>
       <Button>click me</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
